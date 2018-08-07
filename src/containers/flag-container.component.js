6a4bc23895292f708ca5915566aa72ee72b86998
Krystian Bieniek <krystekk2013@gmail.com>
2018-08-07 15:12:08 +0200

    Add functionalism to app

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -27,7 +27,7 @@ class CountryFlagContainer extends Component {
         <div className="search text-center">
           <input type="text" onChange={this.search.bind(this)}/>
         </div>
-        <CountryFlagList countries={this.props.visibleCountries} />
+        <CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)} />
       </div>
     )
   }
