469c58e8269b594be2cdb82810e8e293372d4572
Krystian Bieniek <krystekk2013@gmail.com>
2018-08-09 12:04:12 +0200

    Add world map

--- src/presentational/home.component.js
+++ src/presentational/home.component.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import './country.css';
 
 const Home = () => (
-  <h1>Witamy w aplikacji państwa.js</h1>
+  <div className="world">  
+    <h1>Witamy w aplikacji Państwa.js</h1>
+    <img className="world-map" src="https://uwalls.pl/gallery/61/7331_thumb_b600.jpg" alt="world map" />
+  </div>
 );
 
 export default Home;
